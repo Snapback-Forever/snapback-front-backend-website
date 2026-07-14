e82578609bc46f05fd0db2fa2011911a65962bf8
cowboy <snapbackforever2026@proton.me>
2026-07-13 22:21:27 -0700

    yes

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -40,65 +40,89 @@ const Logo = ({ logo, snapped, setLoginModal }) => {
               "transform 0.5s cubic-bezier(0.68,-0.55,0.27,1.55)",
           }}
         >
-          <h1
-            id="snapback"
-            style={{
-              fontFamily: "ui-serif",
-              margin: "6vh 0",
-              zIndex: 3,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexWrap: "nowrap",
-              textAlign: "center",
-              lineHeight: 1,
-              width: "100%",
-              whiteSpace: "nowrap",
-            }}
-          >
-            SNAP
+       <h1
+  id="snapback"
+  style={{
+    fontFamily: "ui-serif",
+    margin: "6vh 0",
+    zIndex: 3,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "nowrap",
+    textAlign: "center",
+    lineHeight: 1,
+    width: "100%",
+    whiteSpace: "nowrap",
+  }}
+>
+  SNAP
 
-            {snapped ? (
-              <span
-                key="backward-b"
-                id="backwards-b"
-                style={{
-                  display: "inline-block",
-                  transform: "scaleX(-1)",
-                }}
-              >
-                B
-              </span>
-            ) : (
-              <span
-                key="forward-b"
-                id="backwards-b"
-                style={{
-                  display: "inline-block",
-                  transform: "scaleX(1)",
-                }}
-              >
-                B
-              </span>
-            )}
+  <span
+    style={{
+      position: "relative",
+      display: "inline-block",
+      width: "0.72em",
+      height: "1em",
+      transformStyle: "preserve-3d",
+      perspective: "500px",
+    }}
+  >
+    <span
+      id="backwards-b"
+      style={{
+        position: "absolute",
+        inset: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backfaceVisibility: "hidden",
+        WebkitBackfaceVisibility: "hidden",
+        transform: snapped ? "rotateY(180deg)" : "rotateY(0deg)",
+        transition:
+          "transform 0.5s cubic-bezier(0.68,-0.55,0.27,1.55)",
+      }}
+    >
+      B
+    </span>
 
-            ACK
+    <span
+      id="backwards-b-reversed"
+      style={{
+        position: "absolute",
+        inset: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "whitesmoke",
+        WebkitTextFillColor: "whitesmoke",
+        backfaceVisibility: "hidden",
+        WebkitBackfaceVisibility: "hidden",
+        transform: snapped ? "rotateY(0deg)" : "rotateY(-180deg)",
+        transition:
+          "transform 0.5s cubic-bezier(0.68,-0.55,0.27,1.55)",
+      }}
+    >
+      B
+    </span>
+  </span>
 
-            <span
-              onClick={() => setClicks((currentClicks) => currentClicks + 1)}
-              className="spin"
-              style={{
-                fontSize: "0.4em",
-                margin: 0,
-                marginLeft: "0.15em",
-                alignSelf: "flex-start",
-                cursor: "pointer",
-                userSelect: "none",
-              }}
-            >
-              ©
-            </span>
-          </h1>
+  ACK
+
+  <span
+    onClick={() => setClicks((currentClicks) => currentClicks + 1)}
+    className="spin"
+    style={{
+      fontSize: "0.4em",
+      marginLeft: "0.15em",
+      alignSelf: "flex-start",
+      cursor: "pointer",
+      userSelect: "none",
+    }}
+  >
+    ©
+  </span>
+</h1>
         </div>
       </section>
     </div>
